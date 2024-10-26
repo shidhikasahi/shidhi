cd71752cccbaeb1985bae559232d83cb84c40902
shidhikasahi <shidhi755@gmail.com>
2024-10-26 13:40:55 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Oct 27, 2023',
+  birthdate: 'Oct 27, 2024',
   name: 'My love Shidhi I love you'
 };
 
